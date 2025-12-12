d151e1c31e562e1131948cf2e225118df505e5d2
zPancho! <panchito.correo11@gmail.com>
2025-12-12 09:47:40 -0400

    Update of author colors, and some modifications

---
+++ author-colors.lua
@@ -0,0 +1,11 @@
+authorColors = {
+    ["Agent X"] = function()
+        djui_hud_set_adjusted_color(222, 130, 44, 255)
+    end,
+	["zPancho!"] = function()
+		djui_hud_set_adjusted_color(23, 255, 0, 255)
+    end,
+    ["NormalX71"] = function()
+		djui_hud_set_adjusted_color(70, 70, 70, 255)
+    end
+}
